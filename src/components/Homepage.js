ebca2790ba856ef62a4af28d3af52275d9e44cac
Candace Johnson <ccj286@nyu.edu>
2018-08-07 17:54:48 -0400

    update files

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 export default class Homepage extends Component {
     render() {
         return(
-            <div className="App">
-            <header className="App-header">
-              <h1 className="App-title">Grades Master</h1>
-            </header>
-            <button>Sign In</button>
-            <button>Sign Up</button>
-          </div>    
+            <div>
+                <header>
+                    <h1>Grades Master</h1>
+                </header>
+
+                <button>Sign In</button>
+                <button>Sign Up</button>
+            </div>    
         );
     }
 }
---
+++ src/components/SignUp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SignUp extends Component {
+    render() {
+        return(
+            <div>
+                <h1>This is the Sign Up page.</h1>
+            </div>    
+        );
+    }
+}
